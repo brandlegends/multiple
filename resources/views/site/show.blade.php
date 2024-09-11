f508460245a0a23ad9e1df0ef055557cfe55aaab
hendrikus <hendrikus.hpvk@gmail.com>
2024-09-11 15:54:17 +0200

    update blade

--- resources/views/site/show.blade.php
+++ resources/views/site/show.blade.php
@@ -3,17 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $site->domain }}</title>
+    <title>{{ $page->meta_title ?? $page->title }}</title>
+    <meta name="description" content="{{ $page->meta_description }}">
 </head>
 <body>
-    <h1>Posts for {{ $site->domain }}</h1>
-    <ul>
-        @foreach ($posts as $post)
-            <li>
-                <h2>{{ $post->title }}</h2>
-                <p>{{ $post->content }}</p>
-            </li>
-        @endforeach
-    </ul>
+    <h1>{{ $page->title }}</h1>
+    <div>{!! $page->content !!}</div>
 </body>
 </html>
